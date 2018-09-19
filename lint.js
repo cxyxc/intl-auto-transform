799de07e81da646e1f789eae14071870665182f0
崔灿 <cuican@sunlight.bz>
2018-09-19 11:16:31 +0800

    lint.js 添加 babel AST 转化

--- lint.js
+++ lint.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 
+const babel = require('@babel/core');
+const path = require('path');
 const fs = require('file-system');
 const format = require("prettier-eslint");
 const currentDir = process.cwd();
@@ -10,9 +12,20 @@ fs.recurseSync(currentDir, [
     '**/*.jsx',
 ], (filepath, relative, filename) => {
     if(!filename) return;
+
+    // 核心 babel 替换逻辑
+    let {code} = babel.transformFileSync(filepath, {
+        babelrc: false,
+        plugins: [
+            path.join(__dirname, 'node_modules', '@babel/plugin-syntax-object-rest-spread'),
+            path.join(__dirname, 'node_modules', '@babel/plugin-syntax-class-properties'),
+            path.join(__dirname, 'node_modules', 'babel-plugin-syntax-jsx'),
+        ]
+    });
+
     // ESLint 格式化
     const options = {
-        text: fs.readFileSync(filepath).toString(),
+        text: code,
         eslintConfig: require('./eslintrc')
     };
     const formatted = format(options);
