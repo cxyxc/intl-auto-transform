cf9578f4bae68ebc6a4d8c769dc92851eff2044e
崔灿 <cuican@sunlight.bz>
2018-09-25 13:49:10 +0800

    完成

--- trans-plugin.js
+++ trans-plugin.js
@@ -1,10 +1,13 @@
-const manager = require('./manager');
 const utils = require('./utils');
 
 const CallExpression = (t, filename) => path => {
-  if(path.node.callee.type === 'MemberExpression'
-  && path.node.callee.property.name === 'getString') {
-      console.log(path.node.arguments[0].value);
+  if((path.node.callee.type === 'MemberExpression'
+    && path.node.callee.property.name === 'getString') || 
+  (path.node.callee.type === 'Identifier'
+    && path.node.callee.name === 'getString')) {
+      const fileKey = filename.split('.')[0];
+      const value = `${utils.toHump(fileKey)}.${utils.transformStr(path.node.arguments[0].value)}`;
+      path.node.arguments[0].value = value;
   }
 }
 
